bbf233f211aaf8e6bb7f9d745816cef5b6414d00
yeonjunky <yeonjunky@gmail.com>
2020-12-06 01:18:04 +0900

    주석 추가

--- auto-venv.bat
+++ auto-venv.bat
@@ -2,7 +2,8 @@
 
 echo "python venv auto generator started!"
 
-cd C:\Users\admin\AppData\Local\Programs\Python\Python38\Scripts
+@REM deafult python install path
+cd C:\Users\admin\AppData\Local\Programs\Python\Python38\Scripts 
 
 IF EXIST %1\venv (
     echo "The venv folder or virtual environment exists in that path."
